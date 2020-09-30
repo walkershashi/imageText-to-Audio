13302add452353b78d122d3f5c24fee92d76d12c
shashi <skssunny30@gmail.com>
2020-09-30 21:57:46 +0530

    Added recognize_text script to recognize the text embeded in the image

---
+++ scripts/recognize_text.py
@@ -0,0 +1,35 @@
+#import required python modules
+import cv2 # opencv
+import pytesseract # tesseract
+import matplotlib.pyplot as plt
+import urllib.request
+import numpy as np
+import glob
+import os
+
+def recognize_text():
+    org_img = cv2.imread("./images/success.jpg")
+    # Image Resize
+    resize_img = cv2.resize(
+        org_img, None, fx = 2, fy = 2, 
+        interpolation = cv2.INTER_CUBIC
+    )
+
+    # Converting to Gray Scale
+    filtered_grayscale_img = cv2.cvtColor(resize_img, cv2.COLOR_BGR2GRAY)
+
+    # Using Gaussian Blur
+    gaussian_blur_img = cv2.GaussianBlur(filtered_grayscale_img, (5, 5), 0)
+    
+    pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+    text_img = pytesseract.image_to_string(
+        gaussian_blur_img
+    )
+
+    text_img = text_img.replace("\n", " ")
+    print(text_img)
+    return text_img
+
+text = recognize_text()
+print(text)
