657ee9e2a03ccb9c5bf96793fce55def6e71ee17
Shashi Kumar Soni <skssunny30@gmail.com>
2020-11-07 14:12:51 +0530

    Update text_to_speech.py

--- scripts/text_to_speech.py
+++ scripts/text_to_speech.py
@@ -3,8 +3,8 @@ import IPython
 from ibm_watson import TextToSpeechV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-apikey = '3PcEPv1M3BoE7HwCDrfgHfz-dgE_sa_BLrL6pSihFjN2' # your apikey
-url = 'https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/90007e7b-9aef-49b1-825c-98767191dfae' # Your url endpoint
+apikey = '' # your apikey
+url = '' # Your url endpoint
 
 # instantiate the TextToSpeech with apikey and set the service url
 T2S = TextToSpeechV1(IAMAuthenticator(apikey))
